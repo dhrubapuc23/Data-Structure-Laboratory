9e14e01ac5f2a19e6efc982d6a0c2a3c2c28c651
Dhrubajyoti Das <dhrubajyoti1212@gmail.com>
2023-01-18 12:49:55 +0600

    Add files via upload

--- linked_list_1.cpp
+++ linked_list_1.cpp
@@ -20,27 +20,20 @@ void create_node(int N)
 {
     int first_value,n_val;
     Node *tmp,*n_node;
-
     cin>>first_value;
     head->data = first_value;
     head->next = NULL;
-
     tmp = head;
-
     for(int i=2;i<=N;i++)
     {
         n_node = (Node *)malloc(sizeof(Node));
-
         cin>>n_val;
-
         n_node->data = n_val;
         n_node->next = NULL;
-
         tmp->next = n_node;
         tmp = tmp->next;
     }
-
-    printList(head);
+    //printList(head);
 }
 
 void insert_begin(int val)
@@ -53,17 +46,120 @@ void insert_begin(int val)
     printList(head);
 }
 
+void insert_end(int val)
+{
+    Node *newNode,*tmp;
+    tmp = head;
+    newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = val;
+    newNode->next = NULL;
+    while(tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+    printList(head);
+}
 
+void insert_any(int val, int loc)
+{
+    Node *newNode,*tmp;
+    int flag = 0;
+    tmp = head;
+    newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = val;
+    //newNode->next = NULL;
+    while(tmp->next != NULL)
+    {
+        if(tmp->data == loc)
+        {
+            flag = 1;
+            break;
+        }
+        else
+        tmp = tmp->next;
+    }
+
+    if(flag == 0)
+        cout<<"Element not found!"<<endl;
+    else
+    {
+        newNode->next = tmp->next;
+        tmp->next = newNode;
+        printList(head);
+    }
+
+
+}
+
+void delete_beg()
+{
+    head = head->next;
+}
+
+void delete_end()
+{
+    Node *tmp,*prev;
+    tmp = head;
+    while(tmp->next != NULL)
+    {
+        prev = tmp;
+        tmp = tmp->next;
+    }
+
+    prev->next = NULL;
+    cout<<endl;
+    printList(head);
+}
+
+void delete_by_val(int val)
+{
+    Node *tmp,*prev;
+    tmp = head;
+    while(tmp->next != NULL)
+    {
+        if(tmp->next->data == val)
+        {
+             prev = tmp;
+             prev->next = tmp->next->next;
+
+        }
+        else
+        {
+            tmp = tmp->next;
+        }
+
+    }
+
+    //cout<<prev->data<<endl;
+
+   // prev->next = NULL;
+    cout<<endl;
+    printList(head);
+}
 
 int main()
 {
     head = (Node *)malloc(sizeof(Node));
-
     int n;
     cin>>n;
-
     create_node(n);
-    cout<<endl;
+    /*cout<<endl;
     insert_begin(60);
+    cout<<endl;
+    cout<<"Insertion at end"<<endl;
+    insert_end(100);
+    cout<<endl;
+    cout<<"Insertion at any location"<<endl;
+    insert_any(200,20);*/
+
+   /* delete_beg();
+    cout<<endl;
+    cout<<"Delete operation - beginning"<<endl;
+    printList(head);*/
+
+    //delete_end();
+    delete_by_val(30);
+
 
 }
